bb99759e51540fce4d18422e698a51899801e2da
Dat Tran <zrauchz@gmail.com>
2026-01-12 09:36:52 +0100

    feat: Part 1 - Project Setup

---
+++ BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+
+namespace BuberDinner.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+  AuthenticationResult Register(string firstName, string lastName, string email, string password);
+  AuthenticationResult Login(string email, string password);
+}
