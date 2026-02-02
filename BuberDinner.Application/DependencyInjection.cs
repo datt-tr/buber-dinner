4a053b17c60e21c8003bf1cb3eb3d34cdfb047ba
Dat Tran <Dat.Tran@wisetechglobal.com>
2026-02-02 16:08:12 +0100

    feat: Part 8 - Validation Behavior

--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,3 +1,10 @@
+using System.Reflection;
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Common.Behaviors;
+using BuberDinner.Application.Services.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Application;
@@ -7,7 +14,11 @@ public static class DependencyInjection
   public static IServiceCollection AddApplication(this IServiceCollection services)
   {
     services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-    
+
+    services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+    services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        
     return services;
   }
 }
